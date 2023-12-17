d1a03779669ca16a48aaa0b76ca64bdd509c6e24
Miguel <envenezuela2000@gmail.com>
2023-12-17 13:47:17 -0400

    Add context

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -1,5 +1,10 @@
-import { Children, createContext, useState } from "react";
-import { PokeType } from "../interfaces/types";
+import { Children, createContext, useEffect, useState } from "react";
+import { 
+    PokeType,
+    AllPokemonsResult,
+    PokemonsByTypeResult
+ } from "../interfaces/types";
+import axios from "axios";
 
 interface ContextProps {
     types: PokeType[]
@@ -8,22 +13,70 @@ interface ContextProps {
     changeTypeSelected: (type: PokeType) => void
 }
 
-export const PokemonContext = createContex<ContextProps>({} as ContextProps)
+export const PokemonContext = createContext<ContextProps>({} as ContextProps)
 
-const PokemonProvider = ({}) => {
-    let AllPokemonUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
+const PokemonProvider = ({ children }: any) => {
+    let AllPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
 
     const defaultState: PokeType = {
         name: 'All',
-        url: AllPokemonUrl
+        url: AllPokemonsUrl
     }
 
     const [AllPokemons, setAllPokemons] = useState(null)
     const [pokemonsFiltered, setPokemonsFiltered] = useState(null)
 
     const [types, setTypes] = useState([defaultState])
-    const [filterSelected]
-    return <PokemonProvider>{Children}</PokemonProvider>
+    const [filterSelected, setFilteredSelected] = useState(defaultState)
+
+    const changeTypeSelected = async (type: PokeType) => {
+        setFilteredSelected(type)
+
+        const { data } = await axios.get(type?.url!)
+
+        let pokemons = data?.pokemon?.map(
+            ({ pokemon }: PokemonsByTypeResult) => pokemon?.url
+        )
+
+        type.name !== 'All'
+            ? setPokemonsFiltered(pokemons)
+            : setPokemonsFiltered(AllPokemons)
+    }
+
+    const getPokemonsType = async () => {
+        const { data } = await axios.get("https://pokeapi.co/api/v2/type")
+
+        setTypes([...types, ...data.results])
+    }
+
+    const getAllPokemons = async () => {
+        const { data } = await axios.get(AllPokemonsUrl);
+
+        let pokemons = data?.results?.map(
+        (pokemon: AllPokemonsResult) => pokemon?.url
+        );
+
+        setAllPokemons(pokemons);
+        setPokemonsFiltered(pokemons);
+    }
+
+    useEffect(() => {
+        getPokemonsType()
+        getAllPokemons()
+    }, [])
+
+    return (
+        <PokemonContext.Provider
+            value={{
+                types,
+                filterSelected,
+                pokemonsFiltered,
+                changeTypeSelected
+            }}
+        >
+            {Children}
+        </PokemonContext.Provider>
+    )
 }
 
 export default PokemonProvider
