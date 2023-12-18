1042aa393b36dd148f7598105fd6d7c2e2db29a0
Miguel <envenezuela2000@gmail.com>
2023-12-17 20:55:31 -0400

    fix pagination

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -6,15 +6,17 @@ import PokemonCard from "./PokemonCard"
 
 interface Props {
     pokemonsUrls?: string[] | null
-    page?: number
-    perPage?: number
+    page: number
+    perPage: number
 }
 
 const PokemonList = ({pokemonsUrls, page, perPage}: Props) => {
   return (
     <div>
         {
-            pokemonsUrls?.map((pokemonsUrls) => (
+            pokemonsUrls
+            ?.slice((page - 1) * perPage, (page - 1) * perPage + perPage)
+            ?.map((pokemonsUrls) => (
                     <PokemonCard key={pokemonsUrls} url={pokemonsUrls} />
             ))
         }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,32 @@
 import { useContext } from "react"
 import { PokemonContext } from "../context/PokemonContext"
 import PokemonList from "../components/PokemonList"
+import { Pagination } from "../components/Pagination"
+import { usePagination } from "../hooks/usePagination"
 
 const Home = () => {
   const { pokemonsFiltered } = useContext(PokemonContext)
+  const { page, nextPage, previousPage, backToHome} = usePagination()
+
+  let perPage = 10
+
   return (
     <div>
         <header>
             <h1>PokeDex</h1>
         </header>
-        <PokemonList pokemonsUrls={pokemonsFiltered} />
+        <PokemonList 
+          pokemonsUrls={pokemonsFiltered}
+          page={page}
+          perPage={perPage}
+        />
+        <Pagination 
+          page={page}
+          nextPage={nextPage}
+          previousPage={previousPage}
+          perPage={perPage}
+          maxItems={pokemonsFiltered?.length}
+        />
     </div>
   )
 }
